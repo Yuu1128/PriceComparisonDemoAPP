c609dc6506652ec578c479df5a7c62daff866aab
Yuu1128 <2732867447@qq.com>
2024-01-27 02:09:10 -0500

    Create the She Innovates-price comparison project

---
+++ Price Comparison/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Price Comparison
+//
+//  Created by Yuna on 1/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Create new Swift project!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Price Comparison/Price_ComparisonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Price_ComparisonApp.swift
+//  Price Comparison
+//
+//  Created by Yuna on 1/27/24.
+//
+
+import SwiftUI
+
+@main
+struct Price_ComparisonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
